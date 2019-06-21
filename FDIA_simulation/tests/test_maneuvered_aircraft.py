d20e90755872e991dda7f1d1e30839e9b9ae0c4b
Quentin Ducasse <quentin.ducasse@free.fr>
2019-06-21 16:59:33 +0200

    Tests added, NEED REVIEW

---
+++ FDIA_simulation/tests/test_maneuvered_aircraft.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 16:38:04 2019
+
+@author: qde
+"""
+
+import unittest
+from fdia_simulation.models.moving_target import Command
+from fdia_simulation.models.maneuvered_aircraft import ManeuveredAircraft
+
+class aircraft_testCase(unittest.TestCase):
+    def setUp(self):
+        self.headx_cmd_test  = Command('headx',0,0,0)
+        self.headz_cmd_test  = Command('headz',0,0,0)
+        self.vel_cmd_test = Command('vel',0.3,0,0)
+        self.aircraft_test = ManeuveredAircraft(x0 = 0, y0 = 0, z0=0, v0 = 0.3, hx0 = 0, hz0 = 0,
+                                                       command_list = [self.headx_cmd_test,
+                                                                       self.headz_cmd_test,
+                                                                       self.vel_cmd_test])
+
+    def test_initial_x(self):
+        self.assertEqual(self.aircraft_test.x,0)
+
+    def test_initial_y(self):
+        self.assertEqual(self.aircraft_test.y,0)
+
+    def test_initial_z(self):
+        self.assertEqual(self.aircraft_test.z,0)
+
+    def test_initial_vel(self):
+        self.assertEqual(self.aircraft_test.vel,0.3)
+
+    def test_initial_headx(self):
+        self.assertEqual(self.aircraft_test.headx,0)
+
+    def test_initial_headz(self):
+        self.assertEqual(self.aircraft_test.headz,0)
+
+    def test_initial_headingx_command(self):
+        self.assertEqual(self.headx_cmd_test,Command('headx',0,0,0))
+
+    def test_initial_headingz_command(self):
+        self.assertEqual(self.headz_cmd_test,Command('headz',0,0,0))
+
+    def test_initial_velocity_command(self):
+        self.assertEqual(self.vel_cmd_test,Command('vel',0.3,0,0))
+
+    def test_initial_headingx_command_in_commands(self):
+        self.assertEqual(self.aircraft_test.commands['headx'],Command('headx',0,0,0))
+
+    def test_initial_headingz_command_in_commands(self):
+        self.assertEqual(self.aircraft_test.commands['headz'],Command('headz',0,0,0))
+
+    def test_initial_velocity_command_in_commands(self):
+        self.assertEqual(self.aircraft_test.commands['vel'],Command('vel',0.3,0,0))
+
+    def test_change_headingx_command(self):
+        self.aircraft_test.change_headx(10,15)
+        cmd_headx = self.aircraft_test.commands['headx']
+        self.assertEqual(cmd_headx.value,10)
+        self.assertEqual(cmd_headx.steps,15)
+
+    def test_change_headingz_command(self):
+        self.aircraft_test.change_headz(10,15)
+        cmd_headz = self.aircraft_test.commands['headz']
+        self.assertEqual(cmd_headz.value,10)
+        self.assertEqual(cmd_headz.steps,15)
+
+    def test_change_velocity_command(self):
+        self.aircraft_test.change_vel(1,15)
+        cmd_vel  = self.aircraft_test.commands['vel']
+        self.assertEqual(cmd_vel.value,1)
+        self.assertEqual(cmd_vel.steps,15)
+
+    def test_change_command_with_headingx(self):
+        self.aircraft_test.change_command('headx',13,17)
+        cmd_head = self.aircraft_test.commands['headx']
+        self.assertEqual(cmd_head.value,13)
+        self.assertEqual(cmd_head.steps,17)
+
+    def test_change_command_with_headingz(self):
+        self.aircraft_test.change_command('headz',13,17)
+        cmd_head = self.aircraft_test.commands['headz']
+        self.assertEqual(cmd_head.value,13)
+        self.assertEqual(cmd_head.steps,17)
+
+    def test_change_command_with_velocity(self):
+        self.aircraft_test.change_command('vel',5,20)
+        cmd_vel  = self.aircraft_test.commands['vel']
+        self.assertEqual(cmd_vel.value,5)
+        self.assertEqual(cmd_vel.steps,20)
+
+    def test_change_headx_command_effect(self):
+        self.aircraft_test.change_command('headx',5.,20)
+        cmd_headx = self.aircraft_test.commands['headx']
+        for _ in range(20):
+            self.aircraft_test.update()
+        self.assertEqual(self.aircraft_test.headx,-5.)
+        self.assertEqual(cmd_headx.value,5.)
+        self.assertEqual(cmd_headx.steps,0.)
+
+    def test_change_headz_command_effect(self):
+        self.aircraft_test.change_command('headz',5.,20)
+        cmd_headz = self.aircraft_test.commands['headz']
+        for _ in range(20):
+            self.aircraft_test.update()
+        self.assertEqual(self.aircraft_test.headz,-5.)
+        self.assertEqual(cmd_headz.value,5.)
+        self.assertEqual(cmd_headz.steps,0.)
+
+if __name__ == "__main__":
+    unittest.main()
--- FDIA_simulation/tests/test_maneuvered_bicycle.py
+++ FDIA_simulation/tests/test_maneuvered_bicycle.py
@@ -9,61 +9,80 @@ import unittest
 from fdia_simulation.models.moving_target import Command
 from fdia_simulation.models.maneuvered_bicycle import ManeuveredBicycle
 
-class ManeuveredBicycleTestCase(unittest.TestCase):
+class bicycle_testCase(unittest.TestCase):
     def setUp(self):
-        self.headingCommandTest  = Command('head',0,0,0)
-        self.velocityCommandTest = Command('vel',0.3,0,0)
-        self.maneuveredBicycleTest = ManeuveredBicycle(x0 = 0, y0 = 0, v0 = 0.3, h0 = 0,
-                                                       command_list = [self.headingCommandTest,
-                                                                       self.velocityCommandTest])
+        self.head_cmd_test  = Command('head',0,0,0)
+        self.vel_cmd_test = Command('vel',0.3,0,0)
+        self.bicycle_test = ManeuveredBicycle(x0 = 0, y0 = 0, v0 = 0.3, h0 = 0,
+                                                       command_list = [self.head_cmd_test,
+                                                                       self.vel_cmd_test])
 
     def test_initial_x(self):
-        self.assertEqual(self.maneuveredBicycleTest.x,0)
+        self.assertEqual(self.bicycle_test.x,0)
 
     def test_initial_y(self):
-        self.assertEqual(self.maneuveredBicycleTest.y,0)
+        self.assertEqual(self.bicycle_test.y,0)
 
     def test_initial_vel(self):
-        self.assertEqual(self.maneuveredBicycleTest.vel,0.3)
+        self.assertEqual(self.bicycle_test.vel,0.3)
 
     def test_initial_head(self):
-        self.assertEqual(self.maneuveredBicycleTest.head,0)
+        self.assertEqual(self.bicycle_test.head,0)
 
     def test_initial_heading_command(self):
-        self.assertEqual(self.headingCommandTest,Command('head',0,0,0))
+        self.assertEqual(self.head_cmd_test,Command('head',0,0,0))
 
     def test_initial_velocity_command(self):
-        self.assertEqual(self.velocityCommandTest,Command('vel',0.3,0,0))
+        self.assertEqual(self.vel_cmd_test,Command('vel',0.3,0,0))
 
     def test_initial_heading_command_in_commands(self):
-        self.assertEqual(self.maneuveredBicycleTest.commands['head'],Command('head',0,0,0))
+        self.assertEqual(self.bicycle_test.commands['head'],Command('head',0,0,0))
 
     def test_initial_velocity_command_in_commands(self):
-        self.assertEqual(self.maneuveredBicycleTest.commands['vel'],Command('vel',0.3,0,0))
+        self.assertEqual(self.bicycle_test.commands['vel'],Command('vel',0.3,0,0))
 
     def test_change_heading_command(self):
-        self.maneuveredBicycleTest.change_head(10,15)
-        cmd_head = self.maneuveredBicycleTest.commands['head']
+        self.bicycle_test.change_head(10,15)
+        cmd_head = self.bicycle_test.commands['head']
         self.assertEqual(cmd_head.value,10)
         self.assertEqual(cmd_head.steps,15)
 
     def test_change_velocity_command(self):
-        self.maneuveredBicycleTest.change_vel(1,15)
-        cmd_vel  = self.maneuveredBicycleTest.commands['vel']
+        self.bicycle_test.change_vel(1,15)
+        cmd_vel  = self.bicycle_test.commands['vel']
         self.assertEqual(cmd_vel.value,1)
         self.assertEqual(cmd_vel.steps,15)
 
     def test_change_command_with_heading(self):
-        self.maneuveredBicycleTest.change_command('head',13,17)
-        cmd_head = self.maneuveredBicycleTest.commands['head']
+        self.bicycle_test.change_command('head',13,17)
+        cmd_head = self.bicycle_test.commands['head']
         self.assertEqual(cmd_head.value,13)
         self.assertEqual(cmd_head.steps,17)
 
     def test_change_command_with_velocity(self):
-        self.maneuveredBicycleTest.change_command('vel',5,20)
-        cmd_vel  = self.maneuveredBicycleTest.commands['vel']
+        self.bicycle_test.change_command('vel',5,20)
+        cmd_vel  = self.bicycle_test.commands['vel']
         self.assertEqual(cmd_vel.value,5)
         self.assertEqual(cmd_vel.steps,20)
+
+    def test_change_velocity_command_effect(self):
+        self.bicycle_test.change_command('vel',5.,20)
+        cmd_vel = self.bicycle_test.commands['vel']
+        for _ in range(20):
+            self.bicycle_test.update()
+        self.assertEqual(self.bicycle_test.vel,5.)
+        self.assertEqual(cmd_vel.value,5.)
+        self.assertEqual(cmd_vel.steps,0.)
+
+    def test_change_headx_command_effect(self):
+        self.bicycle_test.change_command('head',5.,20)
+        cmd_head = self.bicycle_test.commands['head']
+        for _ in range(20):
+            self.bicycle_test.update()
+        self.assertEqual(self.bicycle_test.head,-5.)
+        self.assertEqual(cmd_head.value,5.)
+        self.assertEqual(cmd_head.steps,0.)
+
 
 if __name__ == "__main__":
     unittest.main()

Correct nees computation for Period Radars

examples/benchmark_2period_radars_imm3.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     x0,y0,z0 = trajectory.initial_position(states)
     # ================================ Radar(s) ================================
     ## Period radars
-    dt_rad1 = 0.05
-    dt_rad2 = 0.2
+    dt_rad1 = 0.2
+    dt_rad2 = 0.05
     # Radar 1: Precision radar
     pradar1 = PeriodRadar(x = -6000, y = 10000, dt = dt_rad1)
 
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
     attacker = CumulativeDriftPeriodAttacker(filter = imm, t0 = 300, time = 2000,
                                    delta_drift = np.array([[0,0,1]]).T,
-                                   radar = pradar1, radar_pos = 0)
+                                   radar = pradar2, radar_pos = 1)
 
 
     # =============================== Benchmark ================================

fdia_simulation/benchmarks/benchmark.py
@@ -37,9 +37,9 @@ class Benchmark(object):
         xs,ys,zs          = states[:,0], states[:,3], states[:,6]
         self.pos_data     = np.array(list(zip(xs,ys,zs)))
         # Check if the filter is an IMM or not
-        self.is_period_radar = False
+        self.radar_is_period = False
         if  isinstance(self.radars[0],PeriodRadar):
-            self.is_period_radar = True
+            self.radar_is_period = True
         self.filter_is_imm = False
         if type(self.radar_filter) == RadarIMM:
             self.filter_is_imm = True
@@ -92,7 +92,7 @@ class Benchmark(object):
 
             # If the radars do not have different data rates, the measurement
             # vector consists of the concatenation of the different measurements
-            if not self.is_period_radar:
+            if not self.radar_is_period:
                 self.measured_values = np.concatenate((self.measured_values,current_measured_values),axis=1)
 
             # If the radars have different data rates, the measurement vector
@@ -121,7 +121,7 @@ class Benchmark(object):
         '''
         if measurements is None:
             # Default values for PeriodRadars
-            if self.is_period_radar:
+            if self.radar_is_period:
                 measurements = self.labeled_values
             # Default values for radars with the same data rates
             else:
@@ -142,14 +142,12 @@ class Benchmark(object):
             current_state = deepcopy(self.radar_filter.x)
             est_states.append(current_state)
             if with_nees:
-                if not self.is_period_radar:
+                if not self.radar_is_period:
                     # The corresponding real state to the estimated one
                     state_id = int(self.radars[0].step * i)
                 else:
                     # The corresponding real state to the estimated one
-                    #### CORRECTION NEEDED!!!
-                    state_id = int(measurement.time)
-                    #### CORRECTION NEEDED!!!
+                    state_id = min(int(measurement.time//Track.DT_TRACK),len(self.states))
                 # Computation of the error between true and estimated states
                 states_tilde = np.subtract(est_states[i],np.reshape(self.states[state_id,:],(-8,1)))
                 nees.append(*(states_tilde.T@inv(self.radar_filter.P)@states_tilde))
@@ -274,5 +272,6 @@ class Benchmark(object):
             print("{0} anomalies out of {1} measurements".format(self.radar_filter.anomaly_counter,len(self.estimated_positions)))
         else:
             for filter in self.radar_filter.filters:
-                print("{0} anomalies out of {1} measurements".format(filter.anomaly_counter,len(self.estimated_positions)))
+                if not(filter.detector is None):
+                    print("{0} anomalies out of {1} measurements".format(filter.anomaly_counter,len(self.estimated_positions)))
         if plot: self.plot()

fdia_simulation/tests/benchmarks/test_benchmark_2period_radars.py
@@ -22,13 +22,11 @@ class Benchmark2PeriodRadarsTestEnv(Benchmark2RadarsTestEnv):
         # Radar definition
         dt_rad1 = 0.6
         self.radar1 = PeriodRadar(x=2000,y=2000,dt=dt_rad1)
-        self.radar1.step = 2
         dt_rad2 = 0.3
         self.radar2 = PeriodRadar(x=1000,y=1000,dt=dt_rad2)
-        self.radar2.step = 1
         self.radars = [self.radar1, self.radar2]
         # States definition
-        self.states = np.array([[i,i/2,i/10]*3 for i in range(100)])
+        self.states = np.array([[i,i/2,i/10]*3 for i in range(225)])
         self.len_elements = int(len(self.states)/self.radar1.step + len(self.states)/self.radar2.step)
 
     def test_gen_data_set(self):
@@ -38,6 +36,7 @@ class Benchmark2PeriodRadarsTestEnv(Benchmark2RadarsTestEnv):
 
     def test_process_filter_correct_estimated_positions(self):
         self.benchmark.gen_data_set()
+        #import pdb; pdb.set_trace()
         self.benchmark.process_filter(with_nees = True)
         self.assertEqual(np.shape(self.benchmark.estimated_positions), (self.len_elements,3))
 